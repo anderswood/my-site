4d2257ec1079891af4b23dc09b248d56f9c35dc7
Anders Wood <anderswood@gmail.com>
2017-07-18 18:41:45 -0600

    try icon stuff

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,21 @@
 import React from 'react';
-
+// import gitHub from './icons/image-round.svg';
+// import Icon from 'svg-react-loader?name=Icon!./icons/image-round.svg';
+import myIcon from './icon-test.js'
 
 export const Card = ( {cardInfo, indexNum} ) => {
 
   if(cardInfo.cardType === 'img-card') {
     return (
       <div className={`card card${indexNum} ${cardInfo.cardType}`}>
-        {/* <div>IMG PLACEHOLDER</div> */}
         <div className={`card${indexNum}-hover card-hover`}>
-
-
-
+          <label>
+            { myIcon }
+            {/* <Icon className='my-icon' /> */}
+            {/* <object type="image/svg+xml" data="./icons/image-round.svg" className="logo">
+              Kiwi Log
+            </object> */}
+          </label>
         </div>
       </div>
 
