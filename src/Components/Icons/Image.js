9598d313492962a0ea4ec9af88f43ec6e1c27b1e
Anders Wood <anderswood@gmail.com>
2017-07-19 19:19:52 -0600

    update icons

---
+++ src/Components/Icons/Image.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+  <svg className='card-icon' width='35px' height='35px' fill="#000" xmlnsX="http://ns.adobe.com/Extensibility/1.0/" xmlnsI="http://ns.adobe.com/AdobeIllustrator/10.0/" xmlnsGraph="http://ns.adobe.com/Graphs/1.0/" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="-949 951 100 100">
+    <path d="M-946.5,953.5v95h95v-95H-946.5z M-859.8,961.8v34.6l-18.4-19.9l-28.3,32.7l-13.2-12.7l-18.5,17.5v-52.2H-859.8z M-938.2,1040.2v-14.8l18.5-17.5l13.8,13.2l27.9-32.3l18.2,19.7v31.6H-938.2z"></path>
+    <circle cx="-922.3" cy="978" r="8.4"></circle>
+  </svg>
+)
