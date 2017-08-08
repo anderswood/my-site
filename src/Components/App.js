212a502c5d69550beec67c51511e49d6e1e74484
Anders Wood <anderswood@gmail.com>
2017-08-08 10:11:48 -0600

    sticky nav in progress

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-import { Link, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
+import { Header } from './Header';
 import { CardGrid } from './CardGrid';
 import { AboutMe } from './AboutMe';
-import LinkedIn from './Icons/LinkedIn';
-import Twitter from './Icons/Twitter';
-import Github from './Icons/Github';
 
 class App extends Component {
+
+  componentDidMount () {
+    window.addEventListener('scroll', this.fixNav);
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.fixNav);
+  }
+
+  fixNav () {
+    const headerElem = document.querySelector('.header-container');
+    const heroElem = document.querySelector('.hero-container');
+    const topOfHeader = headerElem.offsetTop;
+    const heightOfHeader = headerElem.offsetHeight;
+    console.log(heightOfHeader);
+    if (window.scrollY >= topOfHeader) {
+      heroElem.style.paddingTop = heightOfHeader + 'px';
+      headerElem.classList.add('fixed-header');
+    } else {
+      headerElem.classList.remove('fixed-header');
+      heroElem.style.paddingTop = 0;
+    }
+    console.log(topOfHeader, window.scrollY);
+  }
+
   render() {
     return (
       <div id="app-container">
-        <div className='header-container'>
+        <div className='hero-container'>
 
-          {/* <header>
-            <h1 id='header-title'>Anders Wood</h1>
-            <h3 id='header-sub-title1' className='sub-title'>Front-End Developer</h3>
-          </header> */}
           <section className='titles-container'>
             <h1 id='title'>Anders Wood</h1>
             <h3 id='sub-title1' className='sub-title'>Front-End Developer</h3>
@@ -24,33 +43,10 @@ class App extends Component {
             <h3 id='sub-title3' className='sub-title'>Adventurer</h3>
           </section>
 
-          <section className='header-icons-container'>
-            <label className='header-label label-linkedIn'>
-              <a className='header-link' href='www.linkedin.com' target="_blank">
-                <LinkedIn />
-              </a>
-            </label>
-            <label className='header-label label-github'>
-              <a className='header-link' href='www.github.com' target="_blank">
-                <Github />
-              </a>
-            </label>
-            <label className='header-label label-twitter'>
-              <a className='header-link' href='www.twitter.com' target="_blank">
-                <Twitter />
-              </a>
-            </label>
-            <label className='header-label label-aboutMe'>
-              <Link to={ '/AboutMe' } className='header-link link-aboutMe'>
-                <h2 className='text-about'>about</h2>
-                <h2 className='text-me'>me</h2>
-              </Link>
-            </label>
-          </section>
-
         </div>
-          <Route path={ '/home' } render={ () => <CardGrid /> }/>
-          <Route path={ '/AboutMe' } render={ () => <AboutMe /> }/>
+        <Header />
+        <Route path={ '/' } render={ () => <CardGrid /> }/>
+        <Route path={ '/AboutMe' } render={ () => <AboutMe /> }/>
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import LinkedIn from './Icons/LinkedIn';
+import Twitter from './Icons/Twitter';
+import Github from './Icons/Github';
+
+export const Header = () => {
+  return(
+    <section className='header-container'>
+
+      <div className='header-left-div'>
+        <h2>Anders Wood</h2>
+        <h3>Front-End Developer</h3>
+      </div>
+
+      <div className='header-right-div'>
+        <label className='header-label label-linkedIn'>
+          <a className='header-link' href='www.linkedin.com' target="_blank">
+            <LinkedIn />
+          </a>
+        </label>
+        <label className='header-label label-github'>
+          <a className='header-link' href='www.github.com' target="_blank">
+            <Github />
+          </a>
+        </label>
+        <label className='header-label label-twitter'>
+          <a className='header-link' href='www.twitter.com' target="_blank">
+            <Twitter />
+          </a>
+        </label>
+        <label className='header-label label-aboutMe'>
+          <Link to={ '/AboutMe' } className='header-link link-aboutMe'>
+            <h2 className='text-about'>about</h2>
+            <h2 className='text-me'>me</h2>
+          </Link>
+        </label>
+      </div>
+
+    </section>
+  )
+}
