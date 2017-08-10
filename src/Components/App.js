94622bdb6dad0a5f39c86e9060ff6e48855847a4
Anders Wood <anderswood@gmail.com>
2017-08-10 14:49:21 -0600

    add snowflake favicon

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import Favicon from 'react-favicon';
 
 import LandingPage from './LandingPage';
 import { Header } from './Header';
@@ -17,6 +18,7 @@ class App extends Component {
   render() {
     return (
       <div id="app-container">
+        <Favicon url={['https://maxcdn.icons8.com/windows10/PNG/512/Holidays/snowflake-512.png']}/>
         <Route exact path='/' render={ (history) => <LandingPage history={ history } />}/>
         <Route exact path='/' render={ () => <CardGrid />}/>
           <Route path='/AboutMe' render={ () => <Header headerStatus='fixed' />} />
